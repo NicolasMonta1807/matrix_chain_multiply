7f405cb1d34c0c2d7bbfb0a705a813b70b03ca68
Nicolás Montañez Velasco <nicolasmonta1807@gmail.com>
2024-09-03 19:57:58 -0500

    fix: result printing

--- main.py
+++ main.py
@@ -14,13 +14,13 @@ def main():
     mem_sol = memoisation.matrix_chain(d)
     end = time.perf_counter_ns()
     print("Memoisation solution: ", mem_sol)
-    print("Time taken: ", end - start)
+    print("Time taken: ", end - start, " ns")
     
     start = time.perf_counter_ns()
     bu_sol = bottom_up.matrix_chain(d)
     end = time.perf_counter_ns()
     print("Bottom-up solution: ", bu_sol)
-    print("Time taken: ", end - start)
+    print("Time taken: ", end - start, " ns")
     
 
 if __name__ == "__main__":
